7982e9d2001114784a69d8f9ea2054bf7074054b
masDO1337 <samod321@gmail.com>
2025-11-06 22:35:23 +0200

    add warning log

--- Logger.js
+++ Logger.js
@@ -5,13 +5,15 @@ function log(message, type = 'log') {
     global.logs.push({ timestamp: timestamp.toISOString(), message, type });
 
     if (type === 'error') console.log(`[${timestamp.toLocaleString("lt-LT")}] \x1b[31m${message}\x1b[0m`);
+    if (type === 'warning') console.log(`[${timestamp.toLocaleString("lt-LT")}] \x1b[33m${message}\x1b[0m`);
     else console.log(`[${timestamp.toLocaleString("lt-LT")}] ${message}`);
 
     // Keep only the latest 100 logs
-    if (global.logs.length > 100) {
+    if (global.logs.length > 200) {
         global.logs.shift();
     }
 }
 
 module.exports = log;
 module.exports.error = (message) => log(message, 'error');
+module.exports.warning = (message) => log(message, "warning");
