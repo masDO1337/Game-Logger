a65eb4cdf134cbea11ce6003b8fbd9984e152968
masDO1337 <samod321@gmail.com>
2025-11-06 09:15:40 +0200

    Update user model

--- DBModels/User.js
+++ DBModels/User.js
@@ -1,20 +1,26 @@
 const mongoose = require('mongoose');
 
+const activity = new mongoose.Schema({
+    name: { type: String, required: true },
+    applicationId: { type: String, required: false },
+    start: { type: Date, required: true }
+}, { _id: false });
+
 const game = new mongoose.Schema({
-  name: { type: String, required: true },
-  applicationId: { type: String, required: false },
-  start: { type: Date, required: true },
-  stop: { type: Date, required: true },
-  createdAt: { type: Date, required: true, default: Date.now }
+    name: { type: String, required: true },
+    applicationId: { type: String, required: false },
+    start: { type: Date, required: true },
+    stop: { type: Date, required: true },
+    createdAt: { type: Date, required: true }
 });
 
 const userSchema = new mongoose.Schema({
   userId: { type: String, required: true, immutable: true },
   password: { type: String, required: false, default: "" },
-  activities: { type: Array, required: false, default: [] },
+  activities: [activity],
   history: [game],
   historyStatus: { type: Array, required: false, default: [] },
-  refreshToken: { type: String, required: false },
+  refreshToken: { type: String, required: false, default: "" },
   role: { type: String, required: true, default: "anonymous" },
 });
 
